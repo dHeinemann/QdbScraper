d3151674613fb21154f83dea7db5e5c519ad1ac1
David Heinemann <davideheinemann@gmail.com>
2011-04-25 12:42:55 +1000

    Cleaned up code a little.

--- QdbScraper.pl
+++ QdbScraper.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/Perl
+#!/usr/bin/perl
 
 # QdbScraper.pl
 # A QdbS quote database scraper (see www.qdbs.org)
@@ -49,7 +49,7 @@ my $url           = "";
 my $directory     = "";
 my $quoteList     = "quotelist.txt";
 my $pageNo        = 1;
-my $lastPageNo    = 9000;
+my $lastPageNo    = 0;
 my $wait          = 2;
 my $quoteCounter  = 0;
 my $fortune       = 0;
@@ -104,7 +104,6 @@ if ($opt_w)
 	}
 }
 
-
 my $instructions  = <<TEXT;
 Scrape a QdbS-powered quote database.
 
@@ -149,26 +148,28 @@ else
 }
 
 print("Opening $quoteList for write...\n");
-open(QUOTELIST, ">$quoteList") or die("Error: Couldn't open $quoteList for writing.\n");
+open(QUOTELIST, ">$quoteList");
 open(FORTUNE, ">fortune.txt") if $fortune == 1;
 
 my $mech = WWW::Mechanize->new(agent => 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.634.0 Safari/534.16');
 
-while ($pageNo <= $lastPageNo)
+do
 {
 	&scanPage();
 	print("Sleeping for $wait seconds...\n\n");
+	$pageNo++;
 	sleep($wait);
-}
+} while ($pageNo <= $lastPageNo);
+
 close(QUOTELIST);
 close(FORTUNE) if $fortune == 1;
-print("Finished.  $quoteCounter quotes saved.\n");
+print("Finished.  $quoteCounter quotes were saved.\n");
 
 sub getLastPageNo()
 {
 	#Searches the Browse page for the highest available page number
 	my $page = HTML::TokeParser->new(\$mech->{content});
-	print("Finding the total number of pages...\n");
+	print("\nFinding the total number of pages...\n");
 	while (my $tag = $page->get_tag("a"))
 	{
 		if ($tag->[1]{title} and $tag->[1]{title} =~ m/Last Page/gi)
@@ -196,9 +197,9 @@ sub scanPage()
 {
 	#Scan the current page for quotes
 	print("Fetching page $pageNo ($url$pageNo)...\n");
-	$mech->get("$url" . "$pageNo");
+	$mech->get("$url$pageNo");
 
-	&getLastPageNo() if $lastPageNo == 9000;
+	&getLastPageNo() if $lastPageNo == 0;
 	my $page = HTML::TokeParser->new(\$mech->{content});
 
 	print("Looking for quotes...\n");
@@ -232,7 +233,7 @@ sub scanPage()
 
 				open(OUTFILE, ">$id.txt");
 				print(OUTFILE "$text");
-				print(FORTUNE "$text\n\n\t$baseUrl?$id\n%\n");
+				print(FORTUNE "$text\n\n\t$baseUrl?$id\n%\n") if $fortune == 1;
 				close(OUTFILE);
 				print("  saved #$id ($directory/$id.txt)\n");
 				$quoteCounter++;
@@ -244,8 +245,7 @@ sub scanPage()
 			}
 		}
 	}
-	print("Page $pageNo done.\n\n");
-	$pageNo++;
+	print("Completed $pageNo of $lastPageNo.\n\n");
 }
 
 sub urlToDirectory()
